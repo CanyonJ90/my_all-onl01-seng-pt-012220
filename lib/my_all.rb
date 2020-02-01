1f34fcf625cf14e2482f0e22035b2c2646e44129
Canyon Mathis <canyon.mathis@gmail.com>
2020-02-01 02:01:48 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -3,11 +3,13 @@
 def my_all?(collection)
   i = 0
   block_return_values = []
-  while i < collection.
-  block_return_values <<
+    while i < collection.
+     block_return_values <<
     yield(collection[1])
     i = i + 1 
   end
+  
+  
    if block_return_values.include?(false)
     false
   else
